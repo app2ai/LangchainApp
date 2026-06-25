7171b652a33215de05b5c24450fe0bc2f254ef96
Vishal Rana <job.vishalrana@gmail.com>
2026-06-25 09:33:17 +0530

    parellal chain file created

---
+++ CHAINS/parallel_chain.py
@@ -0,0 +1,13 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "meta-llama/Meta-Llama-3-8B-Instruct",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
